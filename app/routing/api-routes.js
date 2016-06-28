5dcd8a28a34f0fa36107185c8cd1437469577b17
cpowers <cpowers@deloitte.com>
2016-06-28 00:13:28 -0400

    i'm able to use my handlebars file to send dynamic emails with nodemailer-express-handlebars

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -9,30 +9,69 @@ var waitListData 	= require('../data/waitinglist-data.js');
 var path 			= require('path');
 var nodemailer      = require('nodemailer');
 var bodyParser      = require('body-parser');
+var hbs = require('nodemailer-express-handlebars');
+var smtpTransport = require('nodemailer-smtp-transport');
 var parseUrlencoded = bodyParser.urlencoded({ extended: false });
-var smtpTransport = nodemailer.createTransport("SMTP",{
+var options = {
     service: "Gmail",
     auth: {
-        user: "carolinapoloni@gmail.com",
+        user: "",
         pass: ""
     }
-});
+};
+var transporter = nodemailer.createTransport(smtpTransport(options));
 
-
-
+var options2 = {
+     viewEngine: {
+         extname: '.hbs',
+         layoutsDir: 'app/routing/views/email/',
+         defaultLayout : 'email_body',
+         partialsDir : 'views/partials/'
+     },
+     viewPath: 'app/routing/views/email/',
+     extName: '.hbs'
+ };
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
 module.exports = function(app){
-
-	// API GET Requests
-	// Below code handles when users "visit" a page. 
-	// In each of the below cases when a user visits a link 
-	// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table) 
-	// ---------------------------------------------------------------------------
-
+	app.post('/api/tables', function(req, res){         
+        var mailOptions={
+            to : req.body.to,
+            subject : req.body.subject,
+            text : req.body.text,
+            template: 'email_body',
+            context : {
+                phoneNumber : req.body.phoneNumber
+            }
+        }
+        
+        console.log(mailOptions);
+        transporter.use('compile', hbs(options2));
+        
+        transporter.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log('email error ', error);
+        res.end("error");
+        }else{
+            console.log("Message sent: " + response);
+            res.end("sent");
+         }
+        });
+        
+		if(tableData.length < 5 ){
+			tableData.push(req.body);
+			res.json(true); // KEY LINE
+		}
+		else{
+			waitListData.push(req.body);
+			res.json(false); // KEY LINE
+		}
+    });
+    
+    
 	app.get('/api/tables', parseUrlencoded, function(req, res){
        res.json(tableData);
 	});
@@ -40,69 +79,9 @@ module.exports = function(app){
 	app.get('/api/waitlist', function(req, res){
 		res.json(waitListData);
 	});
-    
-
-	// API POST Requests
-	// Below code handles when a user submits a form and thus submits data to the server.
-	// In each of the below cases, when a user submits form data (a JSON object)
-	// ...the JSON is pushed to the appropriate Javascript array
-	// (ex. User fills out a reservation request... this data is then sent to the server...
-	// Then the server saves the data to the tableData array)
-	// ---------------------------------------------------------------------------
-
-	app.post('/api/tables', function(req, res){
-        console.log(req.body);
-        console.log('req.body.to ', req.body.to);
         
-        var mailOptions={
-            to : req.body.to,
-            subject : req.body.subject,
-            text : req.body.text
-        }
-        
-        
-        console.log(mailOptions);
-        
-        smtpTransport.sendMail(mailOptions, function(error, response){
-        if(error){
-            console.log('email error ', error);
-        res.end("error");
-        }else{
-            console.log("Message sent: " + response.message);
-            res.end("sent");
-         }
-        });
-
-        
-     
-		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-		// It will do this by sending out the value "true" have a table 
-		if(tableData.length < 5 ){
-			tableData.push(req.body);
-			res.json(true); // KEY LINE
-		}
-
-		// Or false if they don't have a table
-		else{
-			waitListData.push(req.body);
-			res.json(false); // KEY LINE
-		}
-        
-        
-	});
-
-	// ---------------------------------------------------------------------------
-	// I added this below code so you could clear out the table while working with the functionality.
-	// Don't worry about it!
-
 	app.post('/api/clear', function(req, res){
-		// Empty out the arrays of data
 		tableData = [];
-		waitListData = [];
-
-//		console.log(tableData);
-//         console.log(req.query.costumerEmail);
-		
-        
-	})
+		waitListData = [];        
+	});
 }
